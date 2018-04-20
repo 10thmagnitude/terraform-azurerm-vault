ba6e5de7c94a6d0156eb3e049b4b2f3d22752393
Scott Nowicki <scott@Scotts-MacBook-Pro.local>
2018-04-20 12:24:33 -0500

    added more outputs to make module a bit more UI friendly

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "cluster_ip_addresses" {
   value = "${azurerm_network_interface.vault.*.private_ip_address}"
 }
+
+output "vault_ui_address" {
+  value = "${azurerm_network_interface.vault.0.private_ip_address}"
+}
+
+output "vault_http_api_port" {
+  value = "${var.api_port}"
+}
